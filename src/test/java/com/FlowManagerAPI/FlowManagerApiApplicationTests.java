2a921413e0e8d349a69fc2b932a6043429e16b34
Gabriel <artedarua@gmail.com>
2024-06-30 15:58:37 -0300

    alteracao testes

--- src/test/java/com/FlowManagerAPI/FlowManagerApiApplicationTests.java
+++ src/test/java/com/FlowManagerAPI/FlowManagerApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.FlowManagerAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FlowManagerApiApplication.class)
 class FlowManagerApiApplicationTests {
 
 	@Test
